779bb7ab30c1a041d4108929311aa9b19b1c46db
MohammedAazam <mohammedaazam757@gmail.com>
2023-05-09 08:35:26 +0530

    commit

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,8 +1,9 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import Button from '../components/Button';
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "../components/Button";
+import { AiOutlineLogin } from "react-icons/ai";
 
 const meta: Meta<typeof Button> = {
-  title: 'examples/Button',
+  title: "examples/Button",
   component: Button,
 };
 
@@ -13,5 +14,31 @@ type Story = StoryObj<typeof Button>;
 
 //exporting primary varient of button
 export const Primary: Story = {
+  args: {
+    intent: "primary",
+    fullwidth: true,
+    children: "Primary Label",
+    LeftIcon: <A />
+  },
+  argTypes: {
+    fullwidth: {
+      type: "boolean",
+      defaultValue: false,
+    },
+  },
+};
 
-}
+export const Disabled: Story = {
+  args: {
+    intent: "disabled",
+    fullwidth: true,
+    children: "Disabled Label",
+    LeftIcon: <AiOutlineLogin />,
+  },
+  argTypes: {
+    fullwidth: {
+      type: "boolean",
+      defaultValue: false,
+    },
+  },
+};

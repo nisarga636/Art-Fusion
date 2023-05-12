5e79eab19893bc51a69d93030af7048bc3557c07
MohammedAazam <mohammedaazam757@gmail.com>
2023-05-12 21:30:05 +0530

    added components

---
+++ src/stories/ListBox.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Listboxcomp from "../components/ListBox";
+
+
+const meta: Meta<typeof Listboxcomp> = {
+    title: "ui/Listboxcomp",
+    component: Listboxcomp,
+  };
+
+  export default meta;
+
+  type Story = StoryObj<typeof Listboxcomp>;
+
+
+  export const Listboxcompcenter: Story = {
+  
+  };
